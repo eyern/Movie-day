f9c42c44b24a5ec16cdcb5e0cccfe9205e5d6208
eyern <eyernfuture@gmail.com>
2019-08-20 08:40:38 +0300

    change the app structure

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,17 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
-class Registration(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min = 2, max=20)])
-    email = StringField('Email',validators=[DataRequired(), Email()] )
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('ConfirmPassword', validators=[DataRequired(),EqualTo() ])
-    submit = SubmitField(Sign Up)
-
-class Login(FlaskForm):
-    email = StringField('Email',validators=[DataRequired(), Email()] )
-    remember = BooleanField('Remember me')
-    password = PasswordField('Password', validators=[DataRequired()])
-  
-    submit = SubmitField(Log In)
+class GenreForm(FlaskForm):
+    name = StringField('Add a Genre', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/models.py
+++ app/models.py
@@ -1,29 +1,56 @@
-from datetime import datetime
-from  app import db, login_manager
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
-    image_file = db.Column(db.String, nullable=False, default = 'default.jpg')
-    password = db.Column(db.String, nullable = False)
-    posts = db.relationship('Post', backref = 'author', lazy=True)
+class Movie:
+    '''
+    Movie class to define Movie Objects
+    '''
+
+    def __init__(self,id,title,overview,poster,vote_average,vote_count, trailer):
+        self.id =id
+        self.title = title
+        self.overview = overview
+        self.poster = 'https://image.tmdb.org/t/p/w500/'+ poster
+        self.vote_average = vote_average
+        self.vote_count = vote_count
+        self.trailer = trailer
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    pass_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}','{self.image_file}')"
+        return f'User {self.username}'
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String, nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text,nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __repr__(self):
-        return f"User('{self.title}', '{self.date_posted}','{self.content}')"
+class Genres(db.Model):
+    __tablename__='genres'
+    id=db.Column(db.Integer,primary_key = True)
+    name= db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
+    def __init__(self, name):
+        self.name = name
